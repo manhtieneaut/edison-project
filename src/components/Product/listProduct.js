17e6855605e605e3da0ff1e910e559268d6a5378
TienCNTT <20200892@eaut.edu.vn>
2023-03-12 16:43:26 +0700

    get API for Product

---
+++ src/components/Product/listProduct.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import "./style.css"
+const ListProduct = () => {
+    // const getList = async () => {
+    //     let response = await fetch('https://dummyjson.com/products/');
+    //     let data = await response.json();
+    //     return data.products;
+    // }
+    // getList().then(item => {
+    //     console.log(item);
+
+    // })
+
+    const [post, setPost] = useState();
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                let response = await fetch('https://dummyjson.com/products?limit=2&skip=0&select=title,price,images');
+                let data = await response.json();
+                console.log(data)
+                setPost(data.products);
+            } catch (error) { }
+        };
+
+        getData();
+    }, []);
+
+    return (
+
+        <div className="ListProduct">
+            {post?.map(item => {
+                console.log(item)
+                return (
+                    <div className='item' key={item?.id}>
+                        <img src={item.images[1]} alt='item'></img>
+                        <h4 className='item-title' >{item?.title}</h4>
+                        <p className='item-price'>{item?.price}$</p>
+                        <a className='add-cart' href='/#'>Add to cart</a>
+                    </div>
+                );
+            })}
+        </div>
+    )
+
+}
+export default ListProduct;
--- src/pages/ProductUI/components/body.js
+++ src/pages/ProductUI/components/body.js
@@ -1,3 +1,4 @@
+import ListProduct from "../../../components/Product/listProduct";
 
 const Body = () => {
 
@@ -11,15 +12,7 @@ const Body = () => {
                 <a href="/#" className="arranger"><i class="fas fa-bars"></i></a>
             </div>
             <div className="list-item">
-
-
-                <div className="col">
-                    <img className="item-img" src={require('../assets/images/iphone.jpg')} alt="souvenir" />
-                    <h3>iphone</h3>
-                    <h4>30000$</h4>
-                </div>
-
-
+                <ListProduct/>
             </div>
         </div>
     )
